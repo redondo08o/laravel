f51a61e0cedb4a772d0424f14a90556cf667a06a
redondo08o <jesusre11d@gmail.com>
2022-11-16 22:10:59 -0500

    first commit

---
+++ Plantilla_Nw/resources/views/pages/home/home.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.admin')
+
+@section('content')
+<h1 style="color: orange;">hola mundo</h1>
+<br><br> <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia laudantium aliquid consequuntur itaque eum amet rerum nam. Vitae ut nobis pariatur ipsa, sed corrupti eligendi autem rerum suscipit, est ducimus.</p>
+
+<input type="image" src="" alt="">
+
+<input type="date" name="" id="">
+<br><br><br><br><br><br><b><b>rad2degr
+    <b>random_bytesrbr
+        b
+        random_bytesrbrbr
+        <b>random_bytesrbrrb
+            random_bytesrbrrbr
+            random_bytesrbrbrr
+            random_bytesrbrbbr
+            <br>random_bytesrbrrbbr
+            <br><br><br><br><>><>>>><<<
+
+
+        </b>
+    </b>
+</b></b>
+<input type="button" value=""><br>
+<input type="url" name="" id="">
+<br><br>
+<input type="datetime-local" name="" id="">
+
+<h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius quibusdam consequuntur voluptatum omnis nesciunt quos ut obcaecati deserunt harum maxime est, accusantium, debitis, ducimus sequi fuga quo explicabo eveniet laborum.</h1>
+@endsection
+
