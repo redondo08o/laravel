39eea0fd5cf43ae44354ece9b6dff438fda95f1b
redondo08o <jesusre11d@gmail.com>
2022-12-03 12:02:19 -0500

    kjkkj

---
+++ Plantilla_Nw/app/Models/t_datos_pers.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class t_datos_pers extends Model
+{
+    use HasFactory;
+
+    protected $t_datos_per = [
+        'per_usu_id',
+        'per_fecha_nacimiento',
+        'per_nombre',
+        'per_apellido',
+        'per_edad'
+    ];
+}
--- Plantilla_Nw/database/migrations/2022_12_03_154427_create_t_datos_pers_table.php
+++ Plantilla_Nw/database/migrations/2022_12_03_154427_create_t_datos_pers_table.php
@@ -13,15 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('t_datos_pers', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->unsignedBigInteger('per_usu_id');
+            $table->date('per_fecha_nacimiento');
+            $table->string('per_nombre');
+            $table->string('per_apellido');
+            $table->integer('per_edad');
             $table->timestamps();
+            $table->foreign('per_usu_id')->references('id')->on('usuarios');
         });
     }
 
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('t_datos_pers');
     }
 };
