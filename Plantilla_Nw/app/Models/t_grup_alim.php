be138a54a5c92f035981188cdc31ba712d8e448f
redondo08o <jesusre11d@gmail.com>
2022-12-03 15:22:46 -0500

    act_je

---
+++ Plantilla_Nw/app/Models/t_grup_alim.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class t_grup_alim extends Model
+{
+    use HasFactory;
+
+    protected $t_grup_alim = [
+        'alim_grup_tp',
+        'alim_grup_cho',
+        'alim_grup_lipi2',
+        'alim_grup_proteinas',
+    ];
+}
